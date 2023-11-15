0b111e74554e83a87d5696514a5582ca2ced9f09
nafef60552 <nafef60552@qianhost.com>
2023-11-15 16:34:41 +0200

    added layout

---
+++ src/component/NoteLayout.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet, useParams } from "react-router-dom"
+import { Note } from "../type/Note"
+
+type NoteLayoutProps = {
+  notes: Note[]
+}
+
+export function NoteLayout({ notes }: NoteLayoutProps) {
+  const { id } = useParams()
+  const note = notes.find(n => n.id === id)
+
+  if (note == null) return <Navigate to="/" replace />
+
+  return <Outlet context={note} />
+}
