984fbb1ad5bd6450ff46d8cb4b3b3ac9fd5b551e
nafef60552 <nafef60552@qianhost.com>
2023-11-15 16:34:32 +0200

    added new note

---
+++ src/component/NewNote.tsx
@@ -0,0 +1,22 @@
+import { NoteData } from "../type/Note"
+import { Tag } from "../type/Tag"
+import { NoteForm } from "./NoteForm"
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  )
+}
