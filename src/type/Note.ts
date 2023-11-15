08a36a712148d5f8394ea2acb51de6295d1c1a90
nafef60552 <nafef60552@qianhost.com>
2023-11-15 16:34:48 +0200

    added types

---
+++ src/type/Note.ts
@@ -0,0 +1,21 @@
+import { Tag } from "./Tag"
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
